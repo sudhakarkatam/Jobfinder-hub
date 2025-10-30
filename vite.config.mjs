95404607033646d9e9aa277fdc166a92a633af6a
sudhakarkatam <sudhakarkatam777@gmail.com>
2025-10-30 23:20:06 +0530

    file changes

--- vite.config.mjs
+++ vite.config.mjs
@@ -42,5 +42,8 @@ export default defineConfig({
     host: "0.0.0.0",
     strictPort: true,
     allowedHosts: ['.amazonaws.com', '.builtwithrocket.new']
+  },
+  optimizeDeps: {
+    include: ['react-markdown', 'github-slugger']
   }
 });
