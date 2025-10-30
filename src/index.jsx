c90eaea950457dec26f63c7fba05fdbe38b01705
sudhakarkatam <sudhakarkatam777@gmail.com>
2025-10-31 01:07:17 +0530

    changes done

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './global-normalize.css';
 import './styles/index.css';
 import './styles/tailwind.css';
 import App from './App.jsx';
